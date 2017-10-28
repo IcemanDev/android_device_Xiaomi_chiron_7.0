934933535896eb4b2def2ba9e85f9da610efdf2c
IceMan <cammaratamichele1966@gmail.com>
2017-10-28 11:52:22 +0200

    Update BoardConfig.mk

--- Xiaomi/chiron/BoardConfig.mk
+++ Xiaomi/chiron/BoardConfig.mk
@@ -135,7 +135,7 @@ endif
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
 #TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 #TWHAVE_SELINUX := true
 TW_NO_USB_STORAGE := false
